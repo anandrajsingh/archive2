950c4247a889fe97202bfb70d76edbc4a91b6a56
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2024-12-24 22:31:25 +0530

    next auth

---
+++ next-auth/auth.ts
@@ -0,0 +1,59 @@
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { db } from "./lib/db"
+import { getUserById } from "./data/user"
+import { UserRole } from "@prisma/client"
+
+
+export const {
+    handlers,
+    auth,
+    signIn,
+    signOut
+} = NextAuth({
+    pages: {
+        signIn: "/login",
+        error: "/error"
+    },
+    events: {
+        async linkAccount({user}){
+            await db.user.update({
+                where: {id: user.id},
+                data: {emailVerified: new Date()}
+            })
+        }
+    },
+    callbacks: {
+        // async signIn({user}){
+        //     if(user.id == null) return false;
+        //     const existingUser = await getUserById(user.id);
+
+        //     if(!existingUser || !existingUser.emailVerified){
+        //         return false
+        //     } 
+        //     return true;
+        // },
+        async jwt({ token }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub)
+            if (!existingUser) return token;
+            token.role = existingUser.role;
+            return token
+        },
+        async session({ token, session }) {
+            if (token.sub && session.user) {
+                session.user.id = token.sub
+            }
+
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole;
+            }
+            return session;
+        }
+    },
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt" },
+    ...authConfig
+})
---
+++ next-auth/routes.ts
@@ -0,0 +1,13 @@
+export const publicRoutes = [
+    "/"
+]
+
+export const authRoutes = [
+    "/login",
+    "/signup",
+    "/auth-error"
+]
+
+export const apiPrefix = "/api/auth"
+
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard/settings"
