38c5265b1e0636896a7495d9f93592a585f0b740
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2024-12-24 22:32:57 +0530

    auth error page

--- next-auth/components/auth/login-form.tsx
+++ next-auth/components/auth/login-form.tsx
@@ -12,8 +12,14 @@ import { FormError } from "../form-error"
 import { FormSuccess } from "../form-success"
 import { useState, useTransition } from "react"
 import { login } from "@/actions/login"
+import { useSearchParams } from "next/navigation"
 
 export const LoginForm = () => {
+
+    const searchParams = useSearchParams()
+    const urlError = searchParams.get("error") === "OAuthAccountNotLinked" ?
+    "Email already in user with different provider"
+    : "";
 
     const [error, setError ] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined >("")
@@ -32,8 +38,8 @@ export const LoginForm = () => {
         startTransition(() => {
             login(values)
             .then((data) => {
-                setError(data.error)
-                setSuccess(data.success)
+                setError(data?.error)
+                setSuccess(data?.success)
             })
         })
     }
@@ -72,7 +78,7 @@ export const LoginForm = () => {
                                     )}
                             />
                         </div>
-                        <FormError message={error}/>
+                        <FormError message={error || urlError}/>
                         <FormSuccess message={success}/>
                         
                         <Button type="submit" disabled={isPending} className="w-full">
