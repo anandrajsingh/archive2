8395618570b9f74687bce45f76773fc1e74e3776
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2024-12-24 22:33:32 +0530

    settings page temporary

--- next-auth/app/dashboard/settings/page.tsx
+++ next-auth/app/dashboard/settings/page.tsx
@@ -1,6 +1,21 @@
-const Settings = () => {
+import { auth, signOut } from "@/auth"
+import { Button } from "@/components/ui/button";
+
+
+const Settings = async () => {
+    const session = await auth();
     return (
-        <div>Settings</div>
+        <div>
+            {JSON.stringify(session)}
+            <form action={async () => {
+                "use server"
+                await signOut()
+            }}>
+                <Button>
+                    sign out
+                </Button>
+            </form>
+        </div>
     )
 }
 
