905f715baf8490fb95d639d22fda4ca2d9e57c94
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-01-13 15:29:08 +0530

    fuzzie changes

--- fuzzie/src/app/layout.tsx
+++ fuzzie/src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { DM_Sans, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider ";
+import { ClerkProvider } from "@clerk/nextjs"
 
-const font = DM_Sans({subsets: ['latin']})
+const font = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,14 +17,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={font.className}
-      >
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <html lang="en">
+        <body
+          className={font.className}
+        >
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- next-auth/app/dashboard/page.tsx
+++ next-auth/app/dashboard/page.tsx
@@ -1,7 +1,20 @@
-const Dashboard = () => {
+
+import { auth, signOut } from "@/auth"
+
+export default async function Dashboard (){
+    const session = await auth()
     return (
-        <div>Dashboard</div>
+        <div>
+            Dashboard Page
+            {JSON.stringify(session)}
+            <form action={async () => {
+                "use server"
+                await signOut()
+            }}>
+                <button className="bg-white text-black p-2" type="submit">
+                    Sign OUt
+                </button>
+            </form>
+        </div>
     )
 }
-
-export default Dashboard;
